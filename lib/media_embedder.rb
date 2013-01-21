4be9f3e7f5bf040854f1bc6380f4ef7433141951
Joel Quenneville <joel.quen@gmail.com>
2013-01-21 16:21:57 -0500

    remove print statements

--- lib/media_embedder.rb
+++ lib/media_embedder.rb
@@ -1,8 +1,7 @@
 require 'uri'
 require 'media_embedder/parser'
 require 'media_embedder/hyperlink_resource'
-puts 'before loading directory'
-Dir[File.expand_path("lib/media_embedder/resource/*.rb")].each { |file| puts file; require file }
+Dir[File.expand_path("lib/media_embedder/resource/*.rb")].each { |file| require file }
 
 module MediaEmbedder
 end
